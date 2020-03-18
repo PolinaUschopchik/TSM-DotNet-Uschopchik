3bc7c5f7af99efb2e59e1329d29e7de71508d4aa
PolinaUschopchik <62154509+PolinaUschopchik@users.noreply.github.com>
2020-03-18 18:09:22 +0300

    feat: did a homework number two

---
+++ src/hello world/helloworld.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace TSM_DotNet_Uschopchik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Polina");
+        }
+    }
+}
